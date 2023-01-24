cc55556fa216fe2e2fb5edaea2d3dbdf81c80c35
Matt Turner <matt.turner@lifeway.com>
2023-01-24 15:47:43 -0600

    add docker back and see

--- setup-deps-hashicups.sh
+++ setup-deps-hashicups.sh
@@ -4,13 +4,13 @@ set -eu -o pipefail
 # Install necessary dependencies
 sudo apt-get update
 sudo apt-get -y -qq install ca-certificates curl gnupg lsb-release
-# sudo mkdir -p /etc/apt/keyrings
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-# echo \
-#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-#   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-# sudo apt-get update
-# sudo apt-get install -y -qq docker-ce docker-ce-cli containerd.io docker-compose-plugin git unzip auditd
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y -qq docker-ce docker-ce-cli containerd.io docker-compose-plugin git unzip auditd
 
 # Get HashiCups config
 # git clone https://github.com/hashicorp-demoapp/hashicups-setups
